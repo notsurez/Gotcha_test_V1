b9a6e8817bcd1defe55d079011ca55ae9f94a80f
notsurez <cwcortes42@gmail.com>
2018-09-05 23:40:40 -0400

    Added menus
    Added functions for Main menu and game

--- main.c
+++ main.c
@@ -1,10 +1,45 @@
 #include <stdio.h>
 
+int game_state = 1; //Variable that keeps track of the game state
+
 int
 main(void)
 {
- 	printf ("This is a test");
- 	printf("Yeet");
-	printf ("this is also a test");
+    char answer;
+    MainMenu();
+    Game();
  	return(0);
+}
+
+void MainMenu(){
+    char answer;
+    while(game_state == 1)
+    {
+        printf("[//------Main Menu------//] \n\n");
+        printf("Enter the Game Y/N? \n: ");
+        while(game_state == 1)
+        {
+            scanf(" %c", &answer);
+
+            if(answer == 'y')
+            {
+                game_state = 2;
+            }
+            else if(answer == 'n')
+            {
+              return(0);
+            }
+            else
+            {
+                printf("Please Enter y or n \n: ");
+            }
+        }
+    } //End of while for main menu
+}
+
+void Game(){
+    while(game_state == 2){
+        printf("[//------Game------//]");
+        return(0);
+    }
 }
