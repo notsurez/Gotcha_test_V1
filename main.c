f8c210a4c2eddd99fde8d3e092c9ada2f5a7b01f
notsurez <cwcortes42@gmail.com>
2018-09-05 23:43:53 -0400

    Removed pull mistakes

--- main.c
+++ main.c
@@ -5,17 +5,9 @@ int game_state = 1; //Variable that keeps track of the game state
 int
 main(void)
 {
-<<<<<<< HEAD
     char answer;
     MainMenu();
     Game();
-=======
- 	printf ("This is a test");
- 	printf("Yeet");
-	printf ("this is also a test");
-	printf ("yet another test");
->>>>>>> bed9a2c10ffa2a1ca9e8aa6c198ad6dfcdcdd504
- 	return(0);
 }
 
 void MainMenu(){
